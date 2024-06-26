ad082e5371106591bf9ebd67a4554f1662805c6e
Palak Zalavadia <palak.z@SF-CPU-825.local>
2024-06-26 16:09:06 +0530

    86 Creating Documents and Testing the Model

--- Udemy Section-8/server.js
+++ Udemy Section-8/server.js
@@ -29,7 +29,21 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a price'],
   },
 });
-const tour = mongoose.model('Tour', tourSchema);
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error: ', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
