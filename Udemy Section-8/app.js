b35db2b365b5d3bd7dbc96dde275c5646418fac1
Palak Zalavadia <palak.z@SF-CPU-825.local>
2024-06-26 11:57:18 +0530

    Added project from section 6 and 7

---
+++ Udemy Section-8/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan')
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
+const app = express();
+
+
+// 1. Middleware
+console.log();
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'))
+}
+app.use(express.json()); //middleware
+app.use(express.static(`${__dirname}/public`))
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 2. Route Handlers
+
+// 3. Routes
+app.use('/api/v1/tours',tourRouter)
+app.use('/api/v1/users',userRouter)
+// 4. Start server
+
+module.exports=app
