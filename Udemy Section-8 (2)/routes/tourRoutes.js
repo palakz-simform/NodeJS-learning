6a3af8b266ef80e15f05b1b9fe11ea5235599941
Palak Zalavadia <palak.z@simformsolutions.com>
2024-07-02 18:58:17 +0530

    103 Aggregation Pipeline: Unwinding and Projecting

--- Udemy Section-8 (2)/routes/tourRoutes.js
+++ Udemy Section-8 (2)/routes/tourRoutes.js
@@ -4,9 +4,15 @@ const tourController = require('../controllers/tourController');
 const router = express.Router();
 // router.param('id',tourController.checkId)
 
-router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours)
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route('/tour-stats').get(tourController.getTourStats);
+
 router.route
-('/tour-stats').get(tourController.getTourStats)
+('/monthly-plan/:year').get(tourController.getMonthlyPlan)
+
 
 router
   .route('/')
