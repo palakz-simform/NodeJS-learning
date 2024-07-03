907eb3f25ad7560e2de31e8fafbce6224766ed45
Palak Zalavadia <palak.z@simformsolutions.com>
2024-07-03 15:51:50 +0530

    112 Handling Unhandled routes

--- Udemy Section-9/app.js
+++ Udemy Section-9/app.js
@@ -27,6 +27,12 @@ app.use((req, res, next) => {
 // 3. Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-// 4. Start server
+
+app.all('*', (req, res, next)=>{
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
+  })
+})
 
 module.exports = app;
