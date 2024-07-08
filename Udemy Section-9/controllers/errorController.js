c2560bb0903946e84a0f6c87a874b4fc73faa0d7
Palak Zalavadia <palak.z@simformsolutions.com>
2024-07-08 16:19:18 +0530

    121 Handling Mongoose Validation Errors

--- Udemy Section-9/controllers/errorController.js
+++ Udemy Section-9/controllers/errorController.js
@@ -6,13 +6,16 @@ const handleCastErrorDB = (err) => {
 };
 
 const handleDuplicateFieldDB = (err) => {
-  console.log('dsadsad');
   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-  console.log(value);
   const message = `Duplicate field value: ${value}. Please use another value`;
   return new AppError(message, 400);
 };
 
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map(el => el.message)
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -60,6 +63,8 @@ module.exports = (err, req, res, next) => {
     };
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
     sendErrorProd(error, res);
   }
 };
