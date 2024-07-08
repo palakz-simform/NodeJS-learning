5133a7be78d30d5573aa7c48251eaafb3897ac6e
Palak Zalavadia <palak.z@simformsolutions.com>
2024-07-08 11:10:02 +0530

    117 Adding 404 Not Found Errors

--- Udemy Section-9/controllers/tourController.js
+++ Udemy Section-9/controllers/tourController.js
@@ -1,6 +1,7 @@
 // const fs = require('fs');
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
+const AppError = require('../utils/appError');
 const catchAsync = require('./../utils/catchAsync.js');
 
 // const tours = JSON.parse(
@@ -35,6 +36,9 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.id);
   // const tour = await Tour.findOne({_id:req.params.id})
+  if(!tour){
+    return next(new AppError('No tour found with that ID',404))
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -60,6 +64,9 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     new: true,
     runValidators: true,
   });
+  if(!tour){
+    return next(new AppError('No tour found with that ID',404))
+  }
   res.status(200).json({
     status: 'success',
     data: {
@@ -69,7 +76,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+  if(!tour){
+    return next(new AppError('No tour found with that ID',404))
+  }
   res.status(204).json({
     status: 'success',
     data: null,
