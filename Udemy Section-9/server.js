97abb4eb95103c16ed696d35f7862d038691eb61
Palak Zalavadia <palak.z@simformsolutions.com>
2024-07-08 17:08:55 +0530

    123 Catching uncaught exception

--- Udemy Section-9/server.js
+++ Udemy Section-9/server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException',err => {
+  console.log('UNHANDLED EXCEPTION! Shutting Down');
+  console.log(err.name, err.message);
+  process.exit(1);
+})
+
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -20,10 +27,10 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.messages);
   console.log('UNHANDLED REJECTION! Shutting Down');
+  console.log(err.name, err.message);
   server.close(()=>{
     process.exit(1);
   })
 });
-// test
+
