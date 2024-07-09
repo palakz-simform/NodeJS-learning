01267860afef0941ac0bb9bf9730b7292b7bf735
Palak Zalavadia <palak.z@simformsolutions.com>
2024-07-09 13:51:02 +0530

    131 Protecting Tour route with JWT token

--- Udemy Section-10/controllers/authController.js
+++ Udemy Section-10/controllers/authController.js
@@ -51,3 +51,20 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req,res,next)=>{
+    // 1. Getting token and check if its there
+    let token
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+       token = req.headers.authorization.split(' ')[1];
+        console.log(token);
+    }
+    // 2. Verification token
+    if(!token){
+        return next(new AppError('You are not logged in! Please log in to get access', 401))
+    }
+    // 3. Check if user still exits
+
+    // 4. Check if user changed password after the token was issued
+    next()
+})
