1e3ecb00e9e9e1c11414ffc70c10efaef2c095c1
Palak Zalavadia <palak.z@simformsolutions.com>
2024-07-09 18:03:12 +0530

    132 Protecting the route - Implemented handling JWT Errors and verifying token, user exists, changed password after token issued

--- Udemy Section-10/controllers/authController.js
+++ Udemy Section-10/controllers/authController.js
@@ -1,16 +1,13 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('./../utils/catchAsync.js');
 const AppError = require('../utils/appError.js');
 
 const signToken = (id) => {
-  return jwt.sign(
-    { id },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: process.env.JWT_EXPIRES_IN,
-    },
-  );
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
@@ -19,8 +16,9 @@ exports.signup = catchAsync(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt,
   });
-  const token = signToken(newUser._id)
+  const token = signToken(newUser._id);
   res.status(201).json({
     status: 'success',
     token,
@@ -52,19 +50,38 @@ exports.login = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.protect = catchAsync(async (req,res,next)=>{
-    // 1. Getting token and check if its there
-    let token
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
-       token = req.headers.authorization.split(' ')[1];
-        console.log(token);
-    }
-    // 2. Verification token
-    if(!token){
-        return next(new AppError('You are not logged in! Please log in to get access', 401))
-    }
-    // 3. Check if user still exits
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1. Getting token and check if its there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access', 401),
+    );
+  }
+  // 2. Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
-    // 4. Check if user changed password after the token was issued
-    next()
-})
+  // 3. Check if user still exits
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError('The user belonging to this token does no longer exists.'),
+    );
+  }
+
+  // 4. Check if user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again!', 401),
+    );
+  }
+  // Grant access to protected route
+  req.user = currentUser; //We are doing this so that if we want to access the properties accross different middleware then we can store it in req object
+  next();
+});
--- Udemy Section-10/models/userModel.js
+++ Udemy Section-10/models/userModel.js
@@ -31,6 +31,7 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords are not the same!',
     },
   },
+  passwordChangedAt: Date,
 });
 userSchema.pre('save', async function (next) {
   //   Only run the function if password was actually modified
@@ -44,9 +45,24 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
-userSchema.methods.correctPassword = async function(candidatePassword, userPassword){
-    return await bcrypt.compare(candidatePassword, userPassword);
-}
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimeStamp) {
+  if (this.passwordChangedAt) {
+    const changedTimeStamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10,
+    );
+   return JWTTimeStamp < changedTimeStamp;
+  }
+//   false means not changed
+  return false;
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
