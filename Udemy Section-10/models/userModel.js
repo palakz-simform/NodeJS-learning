95898f57b83559ab3c07ff52cc3718248b5770a7
Palak Zalavadia <palak.z@simformsolutions.com>
2024-07-09 10:59:27 +0530

    127 Managing Passwords using bcrypt package

--- Udemy Section-10/models/userModel.js
+++ Udemy Section-10/models/userModel.js
@@ -1,28 +1,47 @@
 const mongoose = require('mongoose');
-const validator = require('validator')
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Please tell us your name']
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowecase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE and does not work on UPDATE!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
     },
-    email:{
-        type: String,
-        required: [true, 'Please provide your email'],
-        unique: true,
-        lowecase: true,
-        validate: [validator.isEmail, 'Please provide a valid email'],
-    },
-    photo:String,
-    password: {
-        type: String,
-        required: [true, 'Please provide a password'],
-        minlength: 8
-    },
-    passwordConfirm:{
-        type: String,
-        required: [true, 'Please confirm your password']
-    }
-})
+  },
+});
+userSchema.pre('save', async function (next) {
+    //   Only run the function if password was actually modified
+    if (!this.isModified('password')) return next();
+
+    //   Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    // Delete Password Confirmed Field
+    this.passwordConfirm = undefined;
+  next();
+});
 
 const User = mongoose.model('User', userSchema);
-module.exports = User
+module.exports = User;
