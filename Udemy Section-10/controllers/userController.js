f763922df356a2032168bee659cc65a39c4d20c2
Palak Zalavadia <palak.z@simformsolutions.com>
2024-07-10 17:05:21 +0530

    138 Updating the current user data: email and name

--- Udemy Section-10/controllers/userController.js
+++ Udemy Section-10/controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError.js');
 const catchAsync = require('./../utils/catchAsync.js');
+
+const filterObj = (obj, ...allowedFields) =>{
+  const newObj = {}
+  Object.keys(obj).forEach(el => {
+    if(allowedFields.includes(el)) newObj[el] = obj[el];
+  })
+  return newObj
+}
 
 exports.createUser = (req, res) => {
   res.status(500).json({
@@ -29,7 +38,7 @@ exports.deleteUser = (req, res) => {
   });
 };
 
-exports.getAllUsers = catchAsync(async(req, res) => {
+exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
 
   res.status(200).json({
@@ -39,5 +48,32 @@ exports.getAllUsers = catchAsync(async(req, res) => {
     data: {
       users,
     },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1. Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for the password updates. Please use /updateMyPass',
+      ),
+    );
+  }
+
+  // 2. Filtered out unwanted field names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3. Update User document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data:{
+      user: updatedUser
+    }
   });
 });
