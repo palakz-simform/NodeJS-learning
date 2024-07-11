ebc7f4a6110d577c4e5682e5a1e5a1b6dd9f69b8
Palak Zalavadia <palak.z@simformsolutions.com>
2024-07-11 12:31:26 +0530

    Preventing parameter pollution with hpp package

--- Udemy Section-10/app.js
+++ Udemy Section-10/app.js
@@ -4,9 +4,10 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
-const globalErrorHandler = require('./controllers/errorController')
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -15,7 +16,7 @@ const app = express();
 // 1. Global Middleware
 
 // Set security HTTP headers
-app.use(helmet())
+app.use(helmet());
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
@@ -26,17 +27,31 @@ if (process.env.NODE_ENV === 'development') {
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!'
-})
-app.use('/api',limiter);
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
-app.use(express.json({limit: '10kb'})); //middleware
+app.use(express.json({ limit: '10kb' })); //middleware
 
 // Data sanitization against NoSQL query innjection
 app.use(mongoSanitize());
 // Data sanitization against XSS
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
@@ -57,10 +72,10 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.all('*', (req, res, next)=>{
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
-})
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 module.exports = app;
