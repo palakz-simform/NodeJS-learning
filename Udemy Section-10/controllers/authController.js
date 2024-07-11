d3f8e91c8a991d5a6abc1c28bc8b8bc4f58fde59
Palak Zalavadia <palak.z@simformsolutions.com>
2024-07-11 10:40:09 +0530

    143 Sending JWT via a cookie

--- Udemy Section-10/controllers/authController.js
+++ Udemy Section-10/controllers/authController.js
@@ -14,14 +14,23 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 100,
+    ), //Converting to milliseconds,
+    httpOnly: true,
+  };
+  if(process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+  user.password = undefined
   res.status(statusCode).json({
     status: 'success',
     token,
     data: {
-      user
+      user,
     },
   });
-}
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -31,7 +40,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
     passwordChangedAt: req.body.passwordChangedAt,
   });
-  createSendToken(newUser,201,res)
+  createSendToken(newUser, 201, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -49,7 +58,7 @@ exports.login = catchAsync(async (req, res, next) => {
     return next(new AppError('Incorrect email or password', 401));
   }
   // 3. If everything is ok, send token to client
-  createSendToken(user ,200,res);
+  createSendToken(user, 200, res);
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -164,23 +173,26 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   // Automatically done using pre save middleware
 
   // 4. Log the user in, send JWT to client
-  createSendToken(user ,200,res);
+  createSendToken(user, 200, res);
 });
 
-exports.updatePassword = catchAsync(async(req, res, next) => {
+exports.updatePassword = catchAsync(async (req, res, next) => {
   // 1. Get user from collection
-  const user = await User.findById(req.user.id).select('+password')
+  const user = await User.findById(req.user.id).select('+password');
 
   // 2. Check if POSTed current password is correct
-  if(!(await user.correctPassword(req.body.passwordCurrent, user.password))) return next(new AppError('Your password does not match the current password'))
+  if (!(await user.correctPassword(req.body.passwordCurrent, user.password)))
+    return next(
+      new AppError('Your password does not match the current password'),
+    );
 
-    // 3. If so, update password
-  user.password = req.body.password
-  user.passwordConfirm = req.body.passwordConfirm
-  await user.save()
+  // 3. If so, update password
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  await user.save();
 
   // If we use findByIdAnd Update then the problem is that password and confirmPasswrod validation will not work also the middleware which encrypts the password wont't work so we should not be using update for password related things
 
   // 4. Log user in, send JWT
-  createSendToken(user ,200,res);
-})
+  createSendToken(user, 200, res);
+});
